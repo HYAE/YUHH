a04bc92ca607bd137ad698673322f91b05d2f726
The cooler programmer <justinohjayel@gmail.com>
2020-12-25 23:21:30 +0800

    Added new stuff
    1. Added comic_generator.py. For now, only able to generate a random xkcd comic.
       https://any-api.com/xkcd_com/xkcd_com/docs/API_Description
    
    2. Added a lock feature to the YUHH commands. Now, only one command should run at a given time.
    
    3. Jingwen is here and wants to be documented

--- YUHH.py
+++ YUHH.py
@@ -6,29 +6,31 @@ import trivia_game as trivia
 import time, os
 import score_handler
 from dotenv import load_dotenv
+import comic_generator as comicgen
 
 client = discord.Client()
 
-#global variables TODO: CHANGE LISTS TO DICTS
+#global variables
 Guessing_Game = {"Ongoing" : False, "Answer" : None, "Chances" : 5, "Channel" : None}
 Trivia_Game = {"Ongoing" : False, "Answer_Index" : 0, "Answers" : [], "Channel" : None}
 Conversation = {"Ongoing" : False, "Channel" : None}
+locked = False
 
 
 @client.event
 async def on_ready():
     for guild in client.guilds:
-        await guild.system_channel.send("yuhhh dudes im here")
+        await guild.system_channel.send("Yuhhh dudes im here")
 
 #for reference, https://discordpy.readthedocs.io/en/latest/migrating.html#sending-messages
 @client.event
 async def on_message(message):
-    
+    if message.author == client.user or locked:
+        return
+
     #remove spaces and lowercase it to make it consistent with commands
     unsanitised_message = message
     message.content = gtd.sanitise(message.content)
-    if message.author == client.user:
-        return
     
     if message.content == "testing":
         channel = message.channel
@@ -39,178 +41,227 @@ async def on_message(message):
 
     #INSULT
     if message.content == "insultme":
-        await message.channel.send(textgen.generate_insult())
-        return
-
+        await insult(message)
+        
     #JOKE
     if message.content == "tellmeajoke":
-        joke = textgen.generate_joke()
-        await message.channel.send(joke[0])
-        time.sleep(3)
-        await message.channel.send(joke[1])
-        return
+        await joke(message)
 
     #INSPIRATIONAL QUOTE
     if message.content == "inspireme":
-        quote = textgen.generate_quote()
-        await message.channel.send("'{}' -{}".format(quote[0], quote[1]))
+        await inspire(message)
 
     #PRINT SCORES
     if message.content == "showmescores":
-        channel = message.channel
-        filepath = os.path.join(os.getcwd(), "scores")
-        score_files = os.listdir(filepath)
-
-        await channel.send(score_handler.print_scores())
-        return
+        await show_scores(message)
     
     #COMPLIMENT
     if message.content == "complimentme" or message.content == "praiseme":
-        channel = message.channel
-        compliment = textgen.generate_compliment()
-
-        await channel.send(compliment)
-
-        def checkfunc(m):
-            if m.channel != channel:
-                return False
-            pattern = "thanks|thankyou|ty|thx|thnks"
-            if re.match(pattern, gtd.sanitise(m.content)):
-                return True
-
-        msg = await client.wait_for_message(timeout = 5, author = message.author ,check=checkfunc)
-        await channel.send('Youre welcome! (nerd)')
-        return
+        await compliment(message)
     
     #PICKUP LINE
     if message.content == "pickmeup":
         await message.channel.send(textgen.generate_pickup_line())
         return
 
-
-    #CONVERSATION
-    if Conversation["Ongoing"]:
-        channel = message.channel
-        if channel != Conversation["Channel"]:
-            return
-
-        
-            
-    
-    
-    if message.content == "talkwithme":
-        Conversation["Ongoing"] = True
+    #COMIC GENERATOR
+    if message.content == "showmeacomic":
+        await show_comic(message)
     
     
     #TRIVIA GAME 
     if Trivia_Game["Ongoing"]:
-        #If the message was not sent in the trivia game channel, continue
-        channel = message.channel
-        if channel != Trivia_Game["Channel"]:
-            return
-
-        answer = message.content
-    
-        #check if answer is from 1-4
-        if re.match("[1-4]",answer) == None or len(answer) > 1:
-            await channel.send("Send only the number, ya nerd. (1,2,3,4)")
-            return
-        
-        correct_ans = Trivia_Game["Answers"][Trivia_Game["Answer_Index"]]
-        print(answer, Trivia_Game["Answer_Index"])
-        if int(answer) - 1 != Trivia_Game["Answer_Index"]:
-            await channel.send("Wrong! The answer was {} ({})".format(Trivia_Game["Answer_Index"] + 1, correct_ans))
-        else:
-            await channel.send("Correct! The answer is {} ({})".format(Trivia_Game["Answer_Index"] + 1, correct_ans))
-
-            #add score
-            score_handler.add_score("Trivia_Game", message.author.name)
-            
-
-        Trivia_Game["Ongoing"] = False
+        await trivia_game(message)
 
     
     if message.content == "trivia":
-        check = checkOngoingGame()
-        if check[0]:
-            await message.channel.send(check[1] + " ongoing")
-            return
-
-        
-        channel = message.channel
-        Trivia_Game["Channel"] = channel
-        await channel.send("Starting Game")
-
-        #get the trivia qns
-        results = trivia.generateQuestion()
-        question = html.unescape(results['question'])
-        
-        #shuffle correct ans into incorrect ones
-        answers = results['incorrect_answers']
-        answers.append(results['correct_answer'])
-        answers = [html.unescape(x) for x in answers]
-        
-        random.shuffle(answers)
-        print(answers, results['correct_answer'])
-        
-        await channel.send("Q:\t" + question)
-        await channel.send("1.\t{}\n2.\t{}\n3.\t{}\n4.\t{}".format(answers[0], answers[1], answers[2], answers[3]))
-
-        Trivia_Game["Ongoing"] = True
-        Trivia_Game["Answers"] = answers
-        Trivia_Game["Answer_Index"] = answers.index(results['correct_answer'])
-        return
+        await trivia_game_start(message)
 
 
     #GUESSING GAME  
     if Guessing_Game["Ongoing"]:
-        #If the message was not sent in the guessing game channel, continue
-        channel = message.channel
-        if channel != Guessing_Game["Channel"]:
-            return
-        answer = gtd.sanitise(message.content)
-        if answer != Guessing_Game["Answer"]:
-            Guessing_Game["Chances"] = Guessing_Game["Chances"] - 1
-            await channel.send("Wrong! " + str(Guessing_Game["Chances"]) + " Chance(s) left")
-            if Guessing_Game["Chances"] <= 0:
-                await channel.send("The answer was " + Guessing_Game["Answer"])
-                Guessing_Game["Ongoing"] = False
-        else:
-            await channel.send("Correct! The answer is " + Guessing_Game["Answer"])
-            #add score
-            score_handler.add_score("Guessing_Game", message.author.name)
-            Guessing_Game["Ongoing"] = False
+        await guessing_game(message)
 
         
     if message.content == "guess":
+        await guessing_game_start(message)
         
-        check = checkOngoingGame()
-        if check[0]:
-            await message.channel.send(check[1] + " ongoing")
-            return
+def lock(func):
+    #stops another command from being run if a command is currently being executed
+    async def inner(message):
+        global locked
+        locked = True
+        await func(message)
+        locked = False
+    return inner
 
-        
-        channel = message.channel
-        Guessing_Game["Channel"] = channel
-        await channel.send("Starting Game")
+@lock
+async def insult(message):
+    await message.channel.send(textgen.generate_insult())
+    return
 
-        #do stuff with img and answer
-        img, name = gtd.getImage()
-        img = img.image
-        Guessing_Game["Answer"] = gtd.sanitise(name)
-        
-        with io.BytesIO() as binary:
-            img.save(binary, 'PNG')
-            binary.seek(0)
-            await channel.send(file=discord.File(fp=binary, filename="image.png"))
+@lock
+async def joke(message):
+    joke = textgen.generate_joke()
+    await message.channel.send(joke[0])
+    time.sleep(3)
+    await message.channel.send(joke[1])
+    return
 
-        await channel.send("What is this image? ({} {} letters)".format(re.sub("[a-z]", "-", name.strip()), str(len(Guessing_Game["Answer"]))))
+@lock
+async def inspire(message):
+    quote = textgen.generate_quote()
+    await message.channel.send("'{}' -{}".format(quote[0], quote[1]))
 
-        Guessing_Game["Ongoing"] = True
-        Guessing_Game["Chances"] = 5
+@lock
+async def show_scores(message):
+    channel = message.channel
+    filepath = os.path.join(os.getcwd(), "scores")
+    score_files = os.listdir(filepath)
+
+    await channel.send(score_handler.print_scores())
+    return
+
+@lock
+async def compliment(message):
+    channel = message.channel
+    compliment = textgen.generate_compliment()
+
+    await channel.send(compliment)
+
+    def checkfunc(m):
+        if m.channel != channel:
+            return False
+        pattern = "thanks|thankyou|ty|thx|thnks"
+        if re.match(pattern, gtd.sanitise(m.content)):
+            return True
+
+    msg = await client.wait_for('message', timeout = 2, author = message.author ,check=checkfunc)
+    await channel.send('Youre welcome! (nerd)')
+    return
+
+@lock
+async def show_comic(message):
+    channel = message.channel
+    comic = comicgen.Comic()
+    img = comic.get_image()
+    #send the comic title
+    await channel.send(comic.get_title())
+    with io.BytesIO() as binary:
+        img.save(binary, 'PNG')
+        binary.seek(0)
+        await message.channel.send(file=discord.File(fp=binary, filename="image.png"))
+
+    await channel.send(comic.get_alt_text())
+
+@lock
+async def trivia_game(message):
+    #If the message was not sent in the trivia game channel, continue
+    channel = message.channel
+    if channel != Trivia_Game["Channel"]:
         return
 
+    answer = message.content
+
+    #check if answer is from 1-4
+    if re.match("[1-4]",answer) == None or len(answer) > 1:
+        await channel.send("Send only the number, ya nerd. (1,2,3,4)")
+        return
+    
+    correct_ans = Trivia_Game["Answers"][Trivia_Game["Answer_Index"]]
+    print(answer, Trivia_Game["Answer_Index"])
+    if int(answer) - 1 != Trivia_Game["Answer_Index"]:
+        await channel.send("Wrong! The answer was {} ({})".format(Trivia_Game["Answer_Index"] + 1, correct_ans))
+    else:
+        await channel.send("Correct! The answer is {} ({})".format(Trivia_Game["Answer_Index"] + 1, correct_ans))
+
+        #add score
+        score_handler.add_score("Trivia_Game", message.author.name)
         
+
+    Trivia_Game["Ongoing"] = False
+
+@lock
+async def trivia_game_start(message):
+    check = checkOngoingGame()
+    if check[0]:
+        await message.channel.send(check[1] + " ongoing")
+        return
+
+    
+    channel = message.channel
+    Trivia_Game["Channel"] = channel
+    await channel.send("Starting Game")
+
+    #get the trivia qns
+    results = trivia.generateQuestion()
+    question = html.unescape(results['question'])
+    
+    #shuffle correct ans into incorrect ones
+    answers = results['incorrect_answers']
+    answers.append(results['correct_answer'])
+    answers = [html.unescape(x) for x in answers]
+    
+    random.shuffle(answers)
+    print(answers, results['correct_answer'])
+    
+    await channel.send("Q:\t" + question)
+    await channel.send("1.\t{}\n2.\t{}\n3.\t{}\n4.\t{}".format(answers[0], answers[1], answers[2], answers[3]))
+
+    Trivia_Game["Ongoing"] = True
+    Trivia_Game["Answers"] = answers
+    Trivia_Game["Answer_Index"] = answers.index(results['correct_answer'])
+    return
+
+@lock
+async def guessing_game(message):
+    #If the message was not sent in the guessing game channel, continue
+    channel = message.channel
+    if channel != Guessing_Game["Channel"]:
+        return
+    answer = gtd.sanitise(message.content)
+    if answer != Guessing_Game["Answer"]:
+        Guessing_Game["Chances"] = Guessing_Game["Chances"] - 1
+        await channel.send("Wrong! " + str(Guessing_Game["Chances"]) + " Chance(s) left")
+        if Guessing_Game["Chances"] <= 0:
+            await channel.send("The answer was " + Guessing_Game["Answer"])
+            Guessing_Game["Ongoing"] = False
+    else:
+        await channel.send("Correct! The answer is " + Guessing_Game["Answer"])
+        #add score
+        score_handler.add_score("Guessing_Game", message.author.name)
+        Guessing_Game["Ongoing"] = False
+
+@lock
+async def guessing_game_start(message):
+    check = checkOngoingGame()
+    if check[0]:
+        await message.channel.send(check[1] + " ongoing")
+        return
+
+    
+    channel = message.channel
+    Guessing_Game["Channel"] = channel
+    await channel.send("Starting Game")
+
+    #do stuff with img and answer
+    img, name = gtd.getImage()
+    img = img.image
+    Guessing_Game["Answer"] = gtd.sanitise(name)
+    
+    with io.BytesIO() as binary:
+        img.save(binary, 'PNG')
+        binary.seek(0)
+        await channel.send(file=discord.File(fp=binary, filename="image.png"))
+
+    await channel.send("What is this image? ({} {} letters)".format(re.sub("[a-z]", "-", name.strip()), str(len(Guessing_Game["Answer"]))))
+
+    Guessing_Game["Ongoing"] = True
+    Guessing_Game["Chances"] = 5
+    return
+
+
+
 def checkOngoingGame():
     #returns True if a game is currently being played, and a string of which game.
     if Guessing_Game["Ongoing"]:
---
+++ comic_generator.py
@@ -0,0 +1,51 @@
+import requests
+import random
+import urllib.request
+from PIL import Image
+
+class Comic:
+
+    def __init__(self):
+        self.data = self.generate_xkcd_comic()
+    
+    def generate_xkcd_comic(self):
+        #ok so they have an endpoint api, but they don't have a way to generate random
+        #comicIDs, nor a way to get the max amount. Wikipedia says they have 2402 as of right now,
+        #so ill set the randint max to 2403. maybe find some nice codey way of getting the max int the future
+
+        comic_ID = random.randint(0, 2403)
+        link = r'https://xkcd.com/{}/info.0.json'.format(comic_ID)
+
+        r = requests.get(url = link)
+        data = r.json()
+
+        return data
+
+    def get_title(self):
+        data = self.data
+        if data is None:
+            print("no comic loaded")
+            return
+
+        return data["title"]
+
+    def get_image(self):
+        #returns a PIL Image. converts the url from data["img"]
+        data = self.data
+        if data is None:
+            print("no comic loaded")
+            return
+
+        
+        img_url = data["img"]
+        img = Image.open(urllib.request.urlopen(img_url))
+
+        return img
+
+    def get_alt_text(self):
+        data = self.data
+        if data is None:
+            print("no comic loaded")
+            return
+
+        return data["alt"]
